c1c4992de3a015e35b2ae1aad5610a981ca0bd4c
usama <ahmarkhan1233456@gmail.com>
2024-04-02 12:47:40 +0500

    AOS Animation Remove

--- src/ServicePage.jsx
+++ src/ServicePage.jsx
@@ -28,28 +28,28 @@ const ServicePage = ({pageTitle, ...otherProps}) => {
     <>
       {/* Banner Section */}
      <header className={mergedHeaderStyle}>
-        <h1 data-aos='fade-right'>{pageTitle}</h1>
-        <h5 data-aos='fade-left'><Link to="/" className="linkStyle"><i class="fa-solid fa-house"></i> Home </Link> | {pageTitle}</h5>
+        <h1>{pageTitle}</h1>
+        <h5><Link to="/" className="linkStyle"><i class="fa-solid fa-house"></i> Home </Link> | {pageTitle}</h5>
     </header>
     
     <div className='mt-4'>
-            <div className="row text-center justify-content-center" data-aos='fade-right'>
+            <div className="row text-center justify-content-center">
                 <div className="col-md-11 col-lg-9 col-xl-7 mb-2">
                     <div className="section-title">
-                        <h2 className="sec-title" data-aos='fade-right'>{headingOne}</h2>
+                        <h2 className="sec-title">{headingOne}</h2>
                     </div>
                 </div>
             </div>
-        <div className='w-75 mx-auto' data-aos='fade-right'><p>{paraOne}</p></div>
+        <div className='w-75 mx-auto'><p>{paraOne}</p></div>
     </div>
 
     {/* Image and Text */}
     <div className='container'>
         <Row style={{ marginTop: '4rem' }}>
-            <Col lg={6} data-aos='fade-right'>
+            <Col lg={6}>
                 <img src={imgOne} alt="Mission" className="img-fluid rounded" />
             </Col>
-            <Col lg={6} data-aos='fade-left'>
+            <Col lg={6}>
                 <h2>{headingTwo}</h2>
                 <p>{paraTwo}</p>
                 <a href="" className="btn btn-success rounded-pill py-2 px-4">Contact Us</a>
@@ -60,19 +60,19 @@ const ServicePage = ({pageTitle, ...otherProps}) => {
     {/* Image overlay */}
     <section className='container mt-4'>
         <div className='mt-4'>
-                <div className="row text-center justify-content-center" data-aos='fade-right'>
+                <div className="row text-center justify-content-center">
                     <div className="col-md-11 col-lg-9 col-xl-7 mb-2">
                         <div className="section-title">
                             <h2 className="sec-title">{headingThree}</h2>
                         </div>
                     </div>
                 </div>
-            <div className='w-75 mx-auto' data-aos='fade-left'><p>{paraThree}</p></div>
+            <div className='w-75 mx-auto'><p>{paraThree}</p></div>
         </div>
         <div className="row">
             {/* 1 */}
             <div className="col-md-4" >
-                <div className="parent-container my-2" data-aos="zoom-in">
+                <div className="parent-container my-2">
                     <img src={imgTwo} alt="Mission" className="img-fluid rounded" />
 
                     <div className="overlay">
@@ -83,7 +83,7 @@ const ServicePage = ({pageTitle, ...otherProps}) => {
             </div>
             {/* 2 */}
             <div className="col-md-4">
-                <div className="parent-container my-2" data-aos="zoom-in">
+                <div className="parent-container my-2">
                     <img src={imgThree} alt="Mission" className="img-fluid rounded" />
 
                     <div className="overlay">
@@ -94,7 +94,7 @@ const ServicePage = ({pageTitle, ...otherProps}) => {
             </div>
             {/* 3 */}
             <div className="col-md-4">
-                <div className="parent-container my-2" data-aos="zoom-in">
+                <div className="parent-container my-2">
                     <img src={imgFour} alt="Mission" className="img-fluid rounded" />
 
                     <div className="overlay">
@@ -109,23 +109,23 @@ const ServicePage = ({pageTitle, ...otherProps}) => {
     
     {/* Text */}
     <div className='mt-4'>
-            <div className="row text-center justify-content-center" data-aos="fade-right">
+            <div className="row text-center justify-content-center">
                 <div className="col-md-11 col-lg-9 col-xl-7 mb-2">
                     <div className="section-title">
                         <h2 className="sec-title">{headingFour}</h2>
                     </div>
                 </div>
             </div>
-        <div className='w-75 mx-auto' data-aos="fade-left"><p>{paraFour}</p></div>
+        <div className='w-75 mx-auto'><p>{paraFour}</p></div>
     </div>
     
     {/* Image and text */}
     <div className='container'>
         <Row style={{ marginTop: '4rem' }}>
-            <Col lg={6} className='order-2 d-flex justify-content-center' data-aos="fade-left">
+            <Col lg={6} className='order-2 d-flex justify-content-center'>
                 <img src={imgFive} alt="Mission" className="img-fluid rounded" />
             </Col>
-            <Col lg={6} className='order-1' data-aos="fade-right">
+            <Col lg={6} className='order-1'>
                 <h2>{headingFive}</h2>
                 <p>{paraFive}</p>
             </Col>
