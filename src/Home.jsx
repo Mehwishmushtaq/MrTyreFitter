c946671402c7b230fef51d93cffc538ed6f9c1b0
usama <ahmarkhan1233456@gmail.com>
2024-04-08 11:21:04 +0500

    "Update Home page with new layout for brands section and add form select for car model; update Navbar with dropdown for services"
    This commit updates the Home page by modifying the brands section to use individual image columns with the img-fluid class, and adding a new form select element for car model. It also updates the Navbar to include a dropdown menu for the services page, listing various service options. The style.css file is updated to include new styles for the brands section and about section. The HeroSection.jsx file is updated to include a new row with a form select for car model.

--- src/Home.jsx
+++ src/Home.jsx
@@ -74,16 +74,28 @@ const Home = () => {
         </Row>
       </section>
 
-      <Container className="mt-4">
-      <Row className="brands-section">
-        <Col md={2}><img src={"images/brands/jaguar.png"} alt="brand" /></Col>
-        <Col md={2}><img src={"images/brands/kia.png"} alt="brand" /></Col>
-        <Col md={2}><img src={"images/brands/dodge.png"} alt="brand" /></Col>
-        <Col md={2}><img src={"images/brands/toyota.png"} alt="brand" /></Col>
-        <Col md={2}><img src={"images/brands/hyundai.png"} alt="brand" /></Col>
-        <Col md={2}><img src={"images/brands/subaru.png"} alt="brand" /></Col>
-      </Row>
-     </Container>
+      <Container className="mt-4 brands-section">
+            <Row style={{ marginTop: '6rem' }}>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/jaguar.png"} alt="brand" /></div>
+                </Col>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/kia.png"} alt="brand" /></div>
+                </Col>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/dodge.png"} alt="brand" /></div>
+                </Col>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/toyota.png"} alt="brand" /></div>
+                </Col>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/hyundai.png"} alt="brand" /></div>
+                </Col>
+                <Col xs={6} sm={5} md={4} lg={3} xl={2}>
+                    <div><img className='img-fluid' src={"images/brands/subaru.png"} alt="brand" /></div>
+                </Col>
+            </Row>
+        </Container>
 
      <Testimonial/>
 
