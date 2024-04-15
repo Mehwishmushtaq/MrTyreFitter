91cbaec4496ca717dbc74512ed36cf267e30db5c
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2024-04-15 18:37:01 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ function Navbar() {
     <Topheader/>
 
       <div className="container-fluid main-nav-bar p-0">
-      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'rgba(0, 0, 0, 0.9)', padding: '0.5rem 2rem' }}>
+      <nav className="navbar navbar-expand-lg navbar-light py-0" style={{ backgroundColor: 'rgba(0, 0, 0, 0.705)', padding: '0.5rem 2rem' }}>
           <div className="container">
             <div className="d-flex justify-content-between w-100">
               <div className="d-flex align-items-center">
