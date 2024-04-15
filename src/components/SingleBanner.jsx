e4c304358c402592fca3c1501e16be93cfa2cdbd
Usama Hassan <64201456+itisusama@users.noreply.github.com>
2024-04-15 11:31:07 +0500

    Update SingleBanner.jsx

--- src/components/SingleBanner.jsx
+++ src/components/SingleBanner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBBtn } from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button';
 
 const SingleBanner = () => {
   return (
@@ -12,9 +12,7 @@ const SingleBanner = () => {
                     <div className="single-banner-image text-light p-5 font-monospace">
                         <h4 className='mt-5'>Heading Goes Here</h4>
                         <h4><strong>The Sub Heading</strong><br /><span>of Heading</span></h4>
-                        <MDBBtn color='light' rippleColor='dark'>
-                            Shop Now
-                        </MDBBtn>                    
+                        <Button variant="success mt-3 px-4">Shop Now</Button>                    
                     </div>
                 </div>
             </section>
