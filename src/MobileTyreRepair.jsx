5733dfc04d094e981e43204b79e0bc8bb1acb33a
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2024-04-17 15:57:43 +0500

    Update MobileTyreRepair.jsx

--- src/MobileTyreRepair.jsx
+++ src/MobileTyreRepair.jsx
@@ -14,20 +14,33 @@ const MobileTyreRepair = () => {
         imgFive="/images/img-11.jpg"
         // Text
         pageTitle="Mobile Tyre Repair"
-        headingOne="heading one goes here"
-        headingTwo="heading two goes here"
-        headingThree="heading three goes here"
-        headingFour="heading four goes here"
-        headingFive="heading five goes here"
-        paraOne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate. Quisque suscipit, ligula eu pharetra scelerisque, tortor ligula aliquam felis, sit amet fringilla odio lectus nec nunc. Ut auctor velit sit amet tortor efficitur, vel tincidunt purus pellentesque. Nunc quis odio sit amet velit ullamcorper rhoncus. Vivamus nec purus in elit hendrerit tincidunt. Proin a urna vel sapien vestibulum feugiat. Curabitur nec justo et justo tincidunt tincidunt. Integer in ultricies erat. Vestibulum ac eros eu turpis convallis bibendum ut non quam."
-        paraTwo="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-        Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-        Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex."
-        paraThree="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate."
-        paraFour="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate."
-        paraFive="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-        Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-        Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, mollitia."
+        headingOne="Quick fix the tyre with Mobile Tyre Repair "
+        headingTwo="Rescue your vehicle with tyre repair near me"
+        headingThree="Ensure your journey with mobile car tyre repair"
+        headingFour="Experience the service of Mobile tyre repair in London "
+        headingFive="Emergency 24-hour tyre repair near me "
+        paraOne="Your damaged tyre is a problem for you and getting the car to the garage is also difficult. We understand that a damaged vehicle can disrupt you and your work schedule. This unknown stress comes to you, which affects your work progress, leading to late arrivals, reduced working performance and irritating mood. To solve all your vehicle tyre-related problems, Pro Tyre Fitters has introduced a mobile tyre repair service that quickly fixes your tyre and gets you on your way again.
+        "
+        paraTwo="The tyre repair service near me offers you the best professionals to serve you at the place you are directed, no matter where you are or what time it is. They arrive at your location to address the issue and provide the best possible solution to resolve it permanently. We aim to provide you with services that make your journey more easier, and we have trained professionals to deal with all types of emergencies anytime and anywhere. They know all the knowledge and good practice to solve tyre problems.
+      
+        "
+        paraThree="Mobile car tyre repair solves the problem of punctures, improper functioning of valves, bursts, minor damage and emergencies. Even maintain and replace tyres to make your journey easier. Most tyre problems are also caused by uneven air pressure and manufacturing defects in the tyre. As a tyre completes its life span, tyre-related issues also arise. All the problems lead to unexpected events that put you and your vehicle at risk.
+        "
+        paraFour="Mobile Tyre Repair London takes you beyond your imagination and ensures your journey is safe. Our top priority is to ensure your safety; our experts ensure your safety first, then find the root cause of the problem and provide you with a quick solution. They arrive at your location with a complete tool kit and necessary equipment. Problems related to tyres mainly occur on the side of the road, causing panic and fear for you. Our experts answer your one call, guide you immediately, and send help as soon as possible.
+
+        "
+        paraFive="We care about your comfort and busy schedule; we know that a stranded vehicle disrupts your daily routine. 24-hour tyre repair near me service provides you with the best professional at your home who can solve your tyre-related problem quickly and save you time and energy. Our experts also serve you at your workplace, making it easy to recover the car without going to the garage. We serve you regardless of time and weather. We are at your service 24/7 to make your journey safe.
+        "
+        scardparaOne="We offer you the best mobile tyre repair service that restores your damaged tyre quickly and saves you from the hassle of taking the vehicle to the garage."
+        scardparaTwo="We provide you with professionals who are experts in the service and know all the problems, their solutions, required toolkits and necessary items for the service.
+
+        "
+        scardparaThree="Our professionals reach your directed location to solve your tyre-related problem and resume your journey whether you are at home, office or roadside.
+
+        "
+        scardOne="Immediate recovery"
+        scardTwo="Well-trained professionals "
+        scardThree="Serve at your location"
      />
     </>
   )
