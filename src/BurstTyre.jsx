28ff7a70b31d1c09e01922ae3848670ec968d3b6
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2024-04-17 15:59:43 +0500

    Update BurstTyre.jsx

--- src/BurstTyre.jsx
+++ src/BurstTyre.jsx
@@ -14,20 +14,31 @@ const BurstTyre = () => {
         imgFive="/images/img-11.jpg"
         // Text
         pageTitle="Burst Tyre"
-        headingOne="heading one goes here"
-        headingTwo="heading two goes here"
-        headingThree="heading three goes here"
-        headingFour="heading four goes here"
-        headingFive="heading five goes here"
-        paraOne="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate. Quisque suscipit, ligula eu pharetra scelerisque, tortor ligula aliquam felis, sit amet fringilla odio lectus nec nunc. Ut auctor velit sit amet tortor efficitur, vel tincidunt purus pellentesque. Nunc quis odio sit amet velit ullamcorper rhoncus. Vivamus nec purus in elit hendrerit tincidunt. Proin a urna vel sapien vestibulum feugiat. Curabitur nec justo et justo tincidunt tincidunt. Integer in ultricies erat. Vestibulum ac eros eu turpis convallis bibendum ut non quam."
-        paraTwo="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-        Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-        Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex."
-        paraThree="Lorem ipsumi dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate."
-        paraFour="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate."
-        paraFive="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-        Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-        Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, mollitia."
+        headingOne="24/7 Emergency burst tyre service "
+        headingTwo="Immediately resolve your tyre burst issue"
+        headingThree="Highly trained Professionals deal with tyre blowouts "
+        headingFour="Tyre blowout service at your directed location "
+        headingFive="Ensure your travel with  burst tyre service"
+        paraOne="A burst tyre is one of the most dreaded occurrences that every driver faces once in a lifetime. Burst incidents mostly happen on roadsides, damaging vehicles and causing accidents. Pro Tyre Fitters is a platform that takes care of the passenger; we design the service to cover the tyre burst incident quickly, manage the situation, ensure your safety and provide the best solution to get you to your destination. The site helps you to start your journey again. Our goal is to make your journey smooth and safe.
+        "
+        paraTwo="A tyre burst will bring physical, mental and emotional stress and cause damage to you and your vehicle. This incident can be caused by overloading the vehicle, driving at high speed and driving with bad tyres. It's not easy to handle situations alone; you can't take the car to the garage, that's why we bring the garage to you with all the facilities. Our customer care officer thoroughly guides you on ensuring your safety, and professionals provide burst tyre service beyond your expectations that helps you resume your journey.
+        "
+        paraThree="We understand that an unexpected tyre blowout disrupts your day-to-day operations, your daily commute, office work, business schedule, and family and friends meetings. You may arrive late or miss an event or meeting. That can cause you unexpected stress that affects your health and work performance. We provide you with service from well-trained professionals who specialize in tyre repair services. They know all tyre-related problems and possible solutions.
+        "
+        paraFour="Our professionals respond quickly to your one call and reach your location, no matter where you are and what time it is. They are available 24/7 to serve you beyond time and place. They reached your location in the middle of the night, even in bad weather. We aim to provide a service that ensures your safety and facilitates your journey. They arrive at the location you direct, be it your home, workplace or roadside, to resolve your issue and resume your journey.
+        "
+        paraFive="To resolve the tyre burst situation, our expert reaches your location immediately and solves your problem with the best possible solution. They arrived with a complete tool kit and all necessary equipment. We understand that the sudden appearance of a tyre problem can strain your pocket, and we care about that stress. We offer you the best services at your directed location at the most affordable price with exceptional professionals who will restore your tyre and resume your journey in no time.
+        "
+        scardparaOne="We provide you with the best service to cover the event of a burst tyre and resume your journey towards your destination. Ensure your safety while travelling with our experts.
+        "
+        scardparaTwo="Our professionals are available 24/7 to deal with emergencies of burst tyres, solve them with the best possible solutions and reduce physical, mental and financial stress.
+        "
+        scardparaThree="Regardless of time and space constraints, our experts arrive at your directed location, arriving on the spot with the complete toolkit to fix your tyre problem efficiently.
+
+        "
+        scardOne="Amazing services  "
+        scardTwo="24/7 available "
+        scardThree="Serve you Anywhere, anytime"
      /> 
     </>
   )
