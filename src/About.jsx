459f6a5e02498279a913df11a04f702b2fc4a6df
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2024-04-17 11:58:53 +0500

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -19,11 +19,11 @@ function About() {
         <Row className="text-center justify-content-center">
             <div className="col-md-11 col-lg-9 col-xl-7 mb-2">
                 <div className="section-title">
-                    <h2 className="sec-title">The Heading Goes Here</h2>
+                    <h2 className="sec-title">Choose us as your travel companion</h2>
                 </div>
             </div>
         </Row>
-        <div className='para-width mx-auto'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit fermentum justo. Vestibulum feugiat vehicula elit, at congue orci venenatis nec. Morbi hendrerit mauris vel arcu hendrerit, nec tristique purus auctor. Nullam euismod nunc nec dictum tempus. Integer malesuada felis vel justo aliquet, a tincidunt arcu vulputate. Quisque suscipit, ligula eu pharetra scelerisque, tortor ligula aliquam felis.</p></div>
+        <div className='para-width mx-auto'><p>Pro Tyre Fitters is a platform that provides you with the best services for your tyres that make your journey easier or more convenient. We understand that a weak and damaged vehicle becomes a safety issue for you throughout the journey. To ensure your safety, we provide you with the best professionals who maintain your vehicle for the daily commute and resolve tyre-related issues. Our professionals are highly trained to handle emergencies, tackle the cause of the problem and resolve it permanently. They are well-trained in their service and fully aware of all tyre-related issues and their solutions.</p></div>
      </div>
 
      {/* New Section */}
@@ -70,11 +70,8 @@ function About() {
       <Container className="py-5">
         <Row style={{ marginTop: '4rem' }}>
           <Col lg={6}>
-            <h2>Our Mission</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-            Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-            Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex.
-            Iure autem molestiae perferendis hic nesciunt id eos voluptas deserunt necessitatibus est eum, eaque labore aspernatur reprehenderit itaque totam adipisci vel. Fugit voluptates culpa debitis. Autem cupiditate ipsum suscipit natus?</p>
+            <h2>We are your vehicle caretaker</h2>
+            <p>We are available 24/7 to serve you beyond your expectations. Our experts serve you at your designated location to save you from the physical stress of taking the car to the garage. They immediately reach the spot, control the situation, reduce your stress and solve the problem quickly. They serve you at home to keep you stress-free, fix tyre problems at the office to save your work schedule from disruption, and save you on the roadside. Our experts save you time, energy and money by providing excellent services at your desired location.</p>
           </Col>
           <Col lg={6} className='order-small-1 d-flex justify-content-center'>
             <img src={"images/img-11.jpg"} alt="Mission" className="img-fluid rounded" />
@@ -96,11 +93,8 @@ function About() {
 
         <Row style={{ marginTop: '4rem' }}>
           <Col lg={6} className='order-2'>
-            <h2>Our Team</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus eveniet, praesentium consectetur, temporibus assumenda eius maiores possimus explicabo quos officiis dolorum quod? Vitae harum delectus porro perspiciatis architecto ipsum sed.
-            Suscipit architecto rem voluptatibus quibusdam provident. Error excepturi sunt perspiciatis earum accusantium, cum blanditiis eius similique vero pariatur. Ea debitis explicabo accusamus molestiae ullam unde sunt provident saepe voluptatum animi?
-            Et nulla saepe quis esse ad ut, ea quod earum illum labore sint dolor velit reiciendis voluptatum, nihil quia corrupti, explicabo sit ex rem suscipit! Mollitia voluptatibus iste ab ex.
-            Iure autem molestiae perferendis hic nesciunt id eos voluptas deserunt necessitatibus est eum, eaque labore aspernatur reprehenderit itaque totam adipisci vel. Fugit voluptates culpa debitis. Autem cupiditate ipsum suscipit natus?</p>
+            <h2>Our commitment to serve you regardless of time and space </h2>
+            <p>We aim to be a part of your journey and provide impeccable service that ensures your journey is smooth and easy. Our professionals are ready to handle any tyre-related problem, from flat to burst tyres; they tackle all issues efficiently. They are always prepared to help you no matter the time. They reach the spot even in the middle of the night and in bad weather. They always come with a complete tool kit and essential tyre accessories. They provide a service that helps you resume your journey to your destination and make it more convenient.</p>
           </Col>
           <Col lg={6} className='order-1 d-flex justify-content-center'>
             <img src={"images/img-11.jpg"} alt="Mission" className="img-fluid rounded" />
@@ -111,11 +105,16 @@ function About() {
           <Col lg={6}>
             <h2>Why Choose Us?</h2>
             <ul>
-              <li>Convenient mobile service</li>
-              <li>Wide selection of tyres</li>
-              <li>Expertise and professionalism</li>
-              <li>Competitive pricing</li>
-              <li>Customer satisfaction guarantee</li>
+              <li>Available 24/7 to serve you.</li>
+              <li>A well-trained staff serves you.</li>
+              <li>We offer you affordable services.</li>
+              <li>Respond to your issue promptly.</li>
+              <li>Serve at your designated location.</li>
+              <li>Save you on the side of the road.</li>
+              <li>Serve at your doorstep or workplace.</li>
+              <li>Save your time, energy and money.</li>
+              <li>Ensure the safety of you and your vehicle.</li>
+              <li>Service specialists manage emergencies.</li>
             </ul>
           </Col>
           <Col lg={6}>
