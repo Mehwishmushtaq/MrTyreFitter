7a757c2f3f7b2b3ed85e475d30270d31c1b23977
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2024-04-17 14:17:50 +0500

    Update faqs.jsx

--- src/faqs.jsx
+++ src/faqs.jsx
@@ -14,97 +14,96 @@ export default function WithContactForm() {
   return (
 
     <>
-    {/* Banner Section */}
-    <header className='headerStyle'>
+      {/* Banner Section */}
+      <header className='headerStyle'>
         <h1>Faq Page</h1>
         <h5><Link to="/" className="linkStyle"><i class="fa-solid fa-house"></i> Home </Link> | Faq</h5>
       </header>
-    <MDBContainer style={{maxWidth: '1000px'}}>
-      {/* <h2 className='text-center' style={{fontSize:'50px'}}><strong>Frequently Asked Question</strong></h2> */}
-      <section className="mt-5">
-        <MDBRow>
-          <MDBCol lg="6" md="12" className="mb-4">
-            <div>
-              <p className="mb-1">
-                <strong>Standard length question?</strong>
-              </p>
-              <p className="mb-1">
-                <u>Higlighted short answer.</u> And some kind of detailed list.
-              </p>
-              <ul>
+      <MDBContainer style={{ maxWidth: '1000px' }}>
+        {/* <h2 className='text-center' style={{fontSize:'50px'}}><strong>Frequently Asked Question</strong></h2> */}
+        <section className="mt-5">
+          <MDBRow>
+            <MDBCol lg="6" md="12" className="mb-4">
+              <div>
+                <p className="mb-1">
+                  <strong>How often do I need car tyre maintenance?</strong>
+                </p>
+                <p className="mb-1">
+                  <p>You need to check the health of your tyres once a month.</p>
+                </p>
+                {/* <ul>
                 <li>list item 1</li>
                 <li>list item 2</li>
                 <li>list item 3</li>
-              </ul>
-            </div>
+              </ul> */}
+              </div>
 
-            <div>
-              <p className="mb-1">
-                <strong>Short question?</strong>
-              </p>
-              <p className="mb-1">
-                <u>Higlighted short answer.</u>
-              </p>
-              <p className="mb-1">
-                Second part of the answer with more details.
-              </p>
+              <div>
+                <p className="mb-1">
+                  <strong>Do you offer mobile tyre fitting at my home?</strong>
+                </p>
+                <p className="mb-1">
+                  <p>Yes, we provide mobile tyre fitting service at your home. Our professionals arrive at your location with a complete tool kit.
+                  </p>
+                </p>
+                <p className="mb-1">
+                  Second part of the answer with more details.
+                </p>
+                <p>
+                  Final part of the answer full of detais and Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+
+              <div>
+                <p className="mb-1">
+                  <strong>
+                    Can you recommend the best tyres for my vehicle?
+                  </strong>
+                </p>
+                <p className="mb-1">
+                  <p>Yes, we recommend the best tyres according to your vehicle to make your journey easier.</p>
+                </p>
+                <p className="mb-1">
+                  Second part of the answer with more details.
+                </p>
+                <p>
+                  Final part of the answer full of detais and Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+
+              <div>
+                <p className="mb-1">
+                  <strong>How long does it take you to replace a tyre?</strong>
+                </p>
+                <p className="mb-1">
+                  <p>Our professionals are good at service; it takes 30 minutes to change your old tyre to a new one.</p>
+                </p>
+                <p className="mb-1">
+                  Second part of the answer with more details.
+                </p>
+                <p>
+                  Final part of the answer full of detais and Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+            </MDBCol>
+            <MDBCol lg="6" md="12" className="text-center mb-3">
               <p>
-                Final part of the answer full of detais and Lorem ipsum dolor
-                sit amet consectetur adipisicing elit.
+                <span class="fw-bold">
+                  Still have any questions? Contact us to get your answer!
+                </span>
               </p>
-            </div>
 
-            <div>
-              <p className="mb-1">
-                <strong>
-                  The longest question in this faq. Made up of two sentences in
-                  order to span to the next line?
-                </strong>
-              </p>
-              <p className="mb-1">
-                <u>Higlighted short answer.</u>
-              </p>
-              <p className="mb-1">
-                Second part of the answer with more details.
-              </p>
-              <p>
-                Final part of the answer full of detais and Lorem ipsum dolor
-                sit amet consectetur adipisicing elit.
-              </p>
-            </div>
-
-            <div>
-              <p className="mb-1">
-                <strong>Slightly longer question?</strong>
-              </p>
-              <p className="mb-1">
-                <u>Higlighted short answer.</u>
-              </p>
-              <p className="mb-1">
-                Second part of the answer with more details.
-              </p>
-              <p>
-                Final part of the answer full of detais and Lorem ipsum dolor
-                sit amet consectetur adipisicing elit.
-              </p>
-            </div>
-          </MDBCol>
-          <MDBCol lg="6" md="12" className="text-center mb-3">
-            <p>
-              <span class="fw-bold">
-                Still have any questions? Contact us to get your answer!
-              </span>
-            </p>
-
-            <form>
-              <MDBInput label="Email address" required className="mb-4" />
-              <MDBTextArea rows={4} label="Message" className="mb-4" />
-              <MDBBtn block className="bg-success">Send</MDBBtn>
-            </form>
-          </MDBCol>
-        </MDBRow>
-      </section>
-    </MDBContainer>
+              <form>
+                <MDBInput placeholder="Email address" required className="mb-4" />
+                <MDBTextArea rows={4} placeholder="Message" className="mb-4" />
+                <MDBBtn block className="bg-success w-25">Send</MDBBtn>            </form>
+            </MDBCol>
+          </MDBRow>
+        </section>
+      </MDBContainer>
     </>
   );
 }
